b30700a544266b8efe33c9b5768e7d2e94c6ac90
jonathan-mothe <jonathan.mothe@gmail.com>
2020-06-09 15:53:21 -0300

    adição dos arquivos views e serializers no diretório backend

--- backend/settings.py
+++ backend/settings.py
@@ -25,6 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'core',
 ]
 
 MIDDLEWARE = [
